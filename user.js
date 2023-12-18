b84ba68fe969bcba46551715f2039e20d1e99b19
That Sana.CCI <96754453+Sana201p@users.noreply.github.com>
2023-12-18 09:07:35 -0300

    Update user.js

--- user.js
+++ user.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-module.exports = mongoose.model('User', userSchema);
